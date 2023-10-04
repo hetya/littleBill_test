6466b95102aeaf025c389718d1c83a51ac468eb5
hetya <henrialsace@laposte.net>
2023-10-05 00:46:20 +0200

    add CORS and patch signup function

--- project/back/src/user/user_router.py
+++ project/back/src/user/user_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from mongodb.mongo import Mongo
 from pydantic import BaseModel
 import pprint
@@ -14,19 +14,21 @@ class IUser(BaseModel):
 # def get_id():
 #     return {"id" : 1}
 
-@router.get("/auth/login") # TODO : add jwt
+@router.post("/auth/login") # TODO : add jwt
 def login(user : IUser):
     try:
-        Mongo().get_users_collection().find_one({"login" : user.login, "password" : user.password})
+        user_data = Mongo().get_users_collection().find_one({"login" : user.login, "password" : user.password})
     except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    if user_data is None:
         raise HTTPException(status_code=400, detail="Bad Request : login or password is incorrect")
-    return {"login" : "login"}
+    return {"connected"}
 
 @router.post("/auth/signup")
 def signup( user : IUser): # TODO: add username filter for characters like whitespaces, etc.
-    if user.login is None or user.login == '' or user.login.len() > 120 :
+    if user.login is None or user.login == '' or len(user.login) > 120 :
         raise HTTPException(status_code=400, detail="Bad Request : login is empty or too long")
-    if user.password is None or user.password == ''or user.login.len() > 120:
+    if user.password is None or user.password == ''or len(user.login) > 120:
         raise HTTPException(status_code=400, detail="Bad Request : password is empty or too long")
     new_user = {
         "login" : user.login,
