bd9925fcf1c06529b855e9ecced6b1c104821731
hetya <henrialsace@laposte.net>
2023-10-05 17:08:10 +0200

    finish get_customer_sales endpoint

--- project/back/src/hiboutik/hiboutik_router.py
+++ project/back/src/hiboutik/hiboutik_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from hiboutik.hitboutik_api_class import HiboutikApi
+import math
 # from mongodb.mongo import MONGOCLIENT
 
-# hiboutik_router = APIRouter(prefix="/hiboutik")
+# prefix="/hiboutik"
 router = APIRouter()
 
 @router.get("/search_customers_by_lastName/{lastName}")
@@ -12,15 +13,19 @@ async def get_customers_by_lastName(lastName : str):
     r = HiboutikApi.get('/customers/search/?last_name=' + lastName)
     return r.json()
 
-@router.get("/get_customer_sales/{customer_id}")
-def get_customer_sales(customer_id : int):
-    r = HiboutikApi.get('/customer/' + str(customer_id), {'p' : '1'})
-    return r.json()
+@router.get("/get_customer_sales/{customer_id}/{page_number}")
+def get_customer_sales(customer_id : int, page_number : int):
+    HIBOUTIK_API_NUMBER_OF_SALES_PER_PAGE = 250
+    MY_API_NUMBER_OF_SALES_PER_PAGE = 5
+    if customer_id is None or customer_id == '' or type(customer_id) is not int or customer_id < 0:
+        raise HTTPException(status_code=400, detail="Bad Request : Bad customer_id")
+    if page_number is None or page_number == '' or type(page_number) is not int or page_number < 0:
+        raise HTTPException(status_code=400, detail="Bad Request : Bad page_number")
+    hiboutik_api_page = math.floor(page_number / (HIBOUTIK_API_NUMBER_OF_SALES_PER_PAGE + 1)) + 1
+    r = HiboutikApi.get('/customer/' + str(customer_id) + '/sales/', {'p' : hiboutik_api_page})
+    index_last_sales = ((page_number * MY_API_NUMBER_OF_SALES_PER_PAGE) % HIBOUTIK_API_NUMBER_OF_SALES_PER_PAGE)
+    index_start_sales = index_last_sales - MY_API_NUMBER_OF_SALES_PER_PAGE
+    return r.json()[index_start_sales:index_last_sales]
 
-@router.get("/create_sales/{customer_id}/{quantity}")
-def create_sales(customer_id : int, quantity : int):
-    # for i in range(quantity):
-        # r = HiboutikApi.post('/sales', {'store_id' : 1, 'customer_id' : customer_id, 'currency_code' : 'EUR'})
-    r = HiboutikApi.post('/sales', {'store_id' : 1, 'customer_id' : customer_id, 'currency_code' : 'EUR'})
-    print(r.json())
-    return "sales created"
+
+
