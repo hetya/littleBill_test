49728629074764716c6822a94ef630a1da300860
hetya <henrialsace@laposte.net>
2023-10-05 21:06:16 +0200

    create jwt at login

--- project/back/src/user/user_router.py
+++ project/back/src/user/user_router.py
@@ -1,14 +1,30 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+import os
+import jwt
 import bcrypt
+import datetime
+from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
 from mongodb.mongo import Mongo
-
 
 router = APIRouter()
 
 class IUser(BaseModel):
     login : str
     password : str
+
+def create_jwt(user_id : str):
+    SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 120
+    if SECRET_KEY is None or ALGORITHM is None or ACCESS_TOKEN_EXPIRE_MINUTES is None:
+        raise HTTPException(status_code=500, detail="Internal Server Error 2")
+        return None
+    data = {"sub": user_id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            }
+    return jwt.encode({"exp": ACCESS_TOKEN_EXPIRE_MINUTES}, SECRET_KEY, algorithm=ALGORITHM)
+    
+
 
 @router.post("/auth/login") # TODO : add jwt
 def login(user : IUser):
@@ -18,7 +34,7 @@ def login(user : IUser):
         raise HTTPException(status_code=500, detail="Internal Server Error 1")
     if user_data is None or not bcrypt.checkpw(user.password.encode('utf-8'), user_data['password']):
         raise HTTPException(status_code=400, detail="Bad Request : login or password is incorrect")
-    return {"connected"}
+    return create_jwt(str(user_data['_id']))
 
 @router.post("/auth/signup")
 def signup( user : IUser): # TODO: add username filter for characters like whitespaces, etc.
